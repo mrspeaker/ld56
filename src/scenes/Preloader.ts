5f5a75e3edf560f14885d3a3295f535c3e8c4c07
Mr Speaker <mrspeaker@gmail.com>
2024-09-30 22:35:30 +0800

    init

---
+++ src/scenes/Preloader.ts
@@ -0,0 +1,41 @@
+import { Scene } from "phaser";
+
+export class Preloader extends Scene {
+    constructor() {
+        super("Preloader");
+    }
+
+    init() {
+        this.add.image(512, 384, "background");
+        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+        this.load.on("progress", (progress: number) => {
+            bar.width = 4 + 460 * progress;
+        });
+    }
+
+    preload() {
+        //  Load the assets for the game - Replace with your own assets
+        this.load.setPath("assets");
+
+        this.load.image("logo", "logo.png");
+        this.load.spritesheet("walk", "sheet.png", {
+            frameWidth: 64 * 1,
+            frameHeight: 64 * 1,
+        });
+    }
+
+    create() {
+        this.anims.create({
+            key: "walk",
+            frames: this.anims.generateFrameNumbers("walk", {
+                frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            }),
+            frameRate: 8,
+            repeat: -1,
+        });
+
+        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+        this.scene.start("MainMenu");
+    }
+}
